1cc6ab04be105c543aa3413c725f637554104807
My Name <nobody@itsthe.crabdance.com>
2015-07-18 21:16:55 -0500

    Removing out-of-date comments, updating readme

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ lithpbot
 (yet another) irc bot
 
 This bot is a program to test my little lisp interpreter [Gojira](https://github.com/dragontux/gojira.git).
-It requires that it be installed first. After that, edit main.scm to set your configuration,
-and then just `./main.scm`.
+It requires that it be installed first. After that, edit main.mlisp to set your configuration,
+and then just `gojira main.mlisp`.
--- irc.scm
+++ irc.scm
@@ -4,17 +4,10 @@
 
 (define tcp-sendstrings
   (lambda (sock strs)
-    ;(map (lambda (str) (tcp-writestr sock str))
     (foreach strs
        (lambda (str) (tcp-writestr sock str)))))
 
-;; Connect to an irc server.
-;; Returns a list representing a server connection.
-;;
-;;    host: Domain, IP, hostname, etc. of the server.
-;;          Must be able to be resolved by gethostbyname(3).
-;;    port: The TCP port of the server to connect to.
-;;  config: List of config parameters, defined in main.scm.
+;; Connects to an irc server.
 (define irc-connect
   (lambda (config)
     (define sock (tcp-socket (config :server) (config :port)))
@@ -57,11 +50,3 @@
                    (tcp-writestr sock str)
                    (tcp-writestr sock crlf)))))
 
-;; Test whether a some thing is an irc server.
-;; Returns a boolean which is true if it is a server, false otherwise.
-;;
-;;    serv: thing to test
-(define irc-server?
-  (lambda (serv)
-    (and (list? serv)
-         (eq? (car serv) 'irc-server))))
